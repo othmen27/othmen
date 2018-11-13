c39c692b97959e25fac10c6a5cd8b7bfbbb4d20b
othmen27 <39593870+othmen27@users.noreply.github.com>
2018-11-13 20:00:28 +0100

    Update bot (1).js

--- bot (1).js
+++ bot (1).js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`Just Having fun`,"http://twitch.tv/Chek My Youtube Channel OthmenGamer")
 client.user.setStatus("dnd")
 });
 
